6801e42a3b61dd7219adf2178ef7b5ca846d72e5
Yoichi-Maribao <szk.m.yo1@gmail.com>
2020-10-08 22:20:50 +0900

    omikuji.js スペルミスの訂正

--- omikuji.js
+++ omikuji.js
@@ -5,8 +5,8 @@ var elName = document.getElementById("name");
 username = prompt("お名前を教えて下さい。");
 if(username == ""){
     elName.innerHTML = "名無し";
-}else[
+}else{
     elName.innerHTML = username;
-]
+}
 var rand = Math.floor(Math.random() * 5);
 document.getElementById("result").innerHTML = userresult[rand];
