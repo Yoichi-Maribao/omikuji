847f409cf2be62f3b764d4211cee6e11fe4c9793
Yoichi-Maribao <szk.m.yo1@gmail.com>
2020-10-08 20:27:06 +0900

    modified omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,10 +1,12 @@
 var username;
-var userresult = ["大吉","中吉","小吉", "吉","凶"];
-username = prompt("お名前を教えてください。");
-if(document.getElementById("name") != ""){
-    document.getElementById("name").innerHTML = "名無し";
-}else{
-    document.getElementById("name").innerHTML = username;
-}
-var randNum = Math.floor(Math.random() * 5);
-document.getElementById("result").innerText = userresult[randNum];
+var userresult = ["大吉", "中吉", "小吉", "吉", "凶"];
+var elName = document.getElementById("name");
+
+username = prompt("お名前を教えて下さい。");
+if(username == ""){
+    elName.innerHTML = "名無し";
+}else[
+    elName.innerHTML = username
+]
+var rand = Math.floor(Math.random() * 5);
+document.getElementById("result").innerHTML = userresult[rand];
